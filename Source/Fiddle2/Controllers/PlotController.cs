7407d1b6b8fa234298a7ff4768a0cd05671f4c14
Oystein Bjorke <oystein.bjorke@gmail.com>
2019-10-25 23:40:32 +0200

    add fiddle 2.0

---
+++ Source/Fiddle2/Controllers/PlotController.cs
@@ -0,0 +1,47 @@
+namespace Fiddle2.Controllers
+{
+    using System.IO;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using Microsoft.CodeAnalysis.CSharp.Scripting;
+    using Microsoft.CodeAnalysis.Scripting;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class PlotController : ControllerBase
+    {
+        public async Task<IActionResult> Get(string format, string source, int width = 800, int height = 400)
+        {
+            var options = ScriptOptions.Default.WithReferences(typeof(OxyPlot.PlotModel).Assembly)
+                .WithImports("System", "OxyPlot", "OxyPlot.Axes", "OxyPlot.Series", "OxyPlot.Annotations");
+
+            var result = await CSharpScript.EvaluateAsync(source, options);
+
+            var model = result as OxyPlot.PlotModel;
+
+            var contentType = this.Request.HasFormContentType ? this.Request.ContentType : "image/png";
+            if (format != null && format == "svg") contentType = "image/svg+xml";
+
+            switch (contentType)
+            {
+                case "image/svg+xml":
+                    {
+                        var e = new OxyPlot.SvgExporter { Width = width, Height = height };
+                        var svg = e.ExportToString(model);
+                        return this.Content(svg, "image/svg+xml");
+                    }
+
+                default:
+                    {
+                        var p = new OxyPlot.Core.Drawing.PngExporter { Width = width, Height = height };
+                        var stream = new MemoryStream();
+                        p.Export(model, stream);
+                        stream.Position = 0;
+                        return this.File(stream, "image/png");
+                    }
+            }
+        }
+    }
+}
