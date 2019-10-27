3245f2493b4a2344eb0fd1cd3d8f91bbeb4b757a
Oystein Bjorke <oystein.bjorke@gmail.com>
2019-10-27 11:38:34 +0100

    show exception message

--- Source/Fiddle2/Controllers/PlotController.cs
+++ Source/Fiddle2/Controllers/PlotController.cs
@@ -1,6 +1,8 @@
 namespace Fiddle2.Controllers
 {
+    using System;
     using System.IO;
+    using System.Text.RegularExpressions;
     using System.Threading.Tasks;
 
     using Microsoft.AspNetCore.Mvc;
@@ -14,15 +16,40 @@ namespace Fiddle2.Controllers
     {
         public async Task<IActionResult> Get(string format, string source, int width = 800, int height = 400, string renderer = null)
         {
-            var options = ScriptOptions.Default.WithReferences(typeof(OxyPlot.PlotModel).Assembly)
-                .WithImports("System", "OxyPlot", "OxyPlot.Axes", "OxyPlot.Series", "OxyPlot.Annotations");
-            source = source.Replace("\n", "\r\n");
-            var result = await CSharpScript.EvaluateAsync(source, options);
-
-            var model = result as OxyPlot.PlotModel;
-
             var contentType = this.Request.HasFormContentType ? this.Request.ContentType : "image/png";
             if (format != null && format == "svg") contentType = "image/svg+xml";
+
+            OxyPlot.PlotModel model;
+
+            try
+            {
+                var options = ScriptOptions.Default.WithReferences(typeof(OxyPlot.PlotModel).Assembly)
+                    .WithImports("System", "OxyPlot", "OxyPlot.Axes", "OxyPlot.Series", "OxyPlot.Annotations");
+                source = source.Replace("\n", "\r\n");
+                var result = await CSharpScript.EvaluateAsync(source, options);
+                model = result as OxyPlot.PlotModel;
+            }
+            catch (Exception e)
+            {
+                model = new OxyPlot.PlotModel() { Title = e.GetType().ToString() };
+                var regex = new Regex(@"\((\d*),(\d*)\): error (.*?):(.*)");
+                var match = regex.Match(e.Message);
+                var message = e.Message;
+                if (match.Success)
+                {
+                    model.Subtitle = "on line " + match.Groups[1].Value + " character " + match.Groups[2].Value;
+                    message = match.Groups[4].Value;
+                }
+                model.Axes.Add(new OxyPlot.Axes.LinearAxis() { Position = OxyPlot.Axes.AxisPosition.Left, Minimum = -1, Maximum = 1 });
+                model.Axes.Add(new OxyPlot.Axes.LinearAxis() { Position = OxyPlot.Axes.AxisPosition.Bottom, Minimum = -1, Maximum = 1 });
+                model.Annotations.Add(new OxyPlot.Annotations.TextAnnotation()
+                {
+                    Text = message,
+                    TextPosition = new OxyPlot.DataPoint(0, 0),
+                    TextHorizontalAlignment = OxyPlot.HorizontalAlignment.Center,
+                    TextVerticalAlignment = OxyPlot.VerticalAlignment.Middle
+                });
+            }
 
             switch (contentType)
             {
